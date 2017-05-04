de952b8e2dcdfe8aa322ed1385be8ebe70291f6b
davisjake <lovejakedavis@gmail.com>
2017-05-04 02:53:41 -0700

    Fixed some formatting and error detection, added the start of filtering while sniffing and getting detailed information from individual packets.

--- sniffer.py
+++ sniffer.py
@@ -13,51 +13,62 @@ from scapy.all import *
 #	3 Choose both how many packets to sniff or how long to sniff  packets
 #		* Option 3 will stop sniffing when either time is up or number of packets to sniff is reached
 def sniffer():
+	#if(input("Would you like to sniff packets filtered or unfiltered?").lower() == 'filtered'): filtered = True
+	#else: filtered = False			//Need to implement filtering (Use sniff(filter="UserDefinedFilter")
 	print("Would you like to-\n\t1: Choose how many packets to sniff\n\t2: Choose how long sniffer runs\n\t3: Choose both how many packets to sniff and how long")
 	howToSniff = input("Enter your choice 1[# of Packets], 2[amount of time to sniff], or 3[Both]: ")
 	hasntSniffed = True		# Loop while method hasn't sniffed
-	while(hasntSniffed):
-		if(howToSniff.isdigit() and int(howToSniff) < 4):
+	while hasntSniffed:
+		if howToSniff.isdigit() and int(howToSniff) < 4:
 			howToSniff = int(howToSniff)	# Determine which option user chose
-			if(howToSniff == 1):
+			if howToSniff == 1:
 				numPackets = input("How many packets would you like to sniff? ")
-				if(numPackets.isdigit()):
+				if numPackets.isdigit():
 					packetsSniffed = sniff(count=int(numPackets))
 					hasntSniffed = False
 				else:
 					print("Error: %s is not a digit" % numPackets)
-			elif(howToSniff == 2):
+					howToSniff = str(howToSniff)
+			elif howToSniff == 2:
 				timeToSniff = input("How long would you like to sniff packets? ")
 				if(timeToSniff.isdigit()):
-					packetsSniffed = sniff(count=int(timeToSniff))
+					packetsSniffed = sniff(timeout=int(timeToSniff))
 					hasntSniffed = False
 				else:
 					print("Error: %s is not a digit" % timeToSniff)
-			elif(howToSniff == 3):
+					howToSniff = str(howToSniff)
+			elif howToSniff == 3:
 				numPackets = input("How many packets would you like to sniff? ")
 				timeToSniff = input("How long would you like to sniff packets? ")
 				if(numPackets.isdigit() and timeToSniff.isdigit()):
 					packetsSniffed = sniff(count=int(numPackets), timeout=int(timeToSniff))
 					hasntSniffed = False
 				else:
-					print("Error: %s or %s is not a digit" % numPackets, timeToSniff)
+					print("Error: %s or %s is not a digit" % (numPackets, timeToSniff))
+					howToSniff = str(howToSniff)
 		else:
 			print("Error: %s is an invalid choice, choose a valid operation(1, 2, or 3)." % howToSniff)
-			howToSniff = str(input("Enter your choice: "))
-	print(packetsSniffed.show())
+			howToSniff = input("Enter your choice: ")
+	packetsSniffed.show()
+	return packetsSniffed
 
 # Main loop of program, user can either sniff or quit program
 def main():
-	print('Network Sniffer')
+	print("Network Sniffer")
 	exist = True
 	while exist:
 		print("Would you like to-\n\tSniff: Sniff packets\n\tQuit: quit packet sniffer?")
-		choice = input("Enter your choice [Sniff or Quit]: ")
-		if(choice.lower() == "sniff"):
-			sniffer()
-		elif(choice.lower() == "quit"):
+		sniffOrQuit = input("Enter your choice [Sniff or Quit]: ")
+		if sniffOrQuit.lower() == "sniff":
+			packets = sniffer()
+			#getDetails = True
+			#while(getDetails):
+				#getDetails = input("Would you like to get more information about any of the sniffed packets?(Pick 1 or 2)\n\t1: Yes\n\t2: No")
+				# Need to implement getting details about already sniffed packets.
+				# Use packets, and ask repeatedly ask User what packet and display until user says to stop
+		elif sniffOrQuit.lower() == "quit":
 			print("Network Sniffer has been quit")
 			exist = False
-		else: 
-			print("Error: %s is an invalid operation, choose a valid operation[Sniff or Quit].\n" % choice)
+		else:
+			print("Error: %s is an invalid operation, choose a valid operation[Sniff or Quit]." % sniffOrQuit)
 main()
